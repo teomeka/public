5820bd410a941e62299e9da914207d38a871a722
root <root@cdemo.vps.sitehost.co.nz>
2020-07-10 08:29:52 +1200

    adding vendor files and .env file

---
+++ vendor/craftcms/plugins.php
@@ -0,0 +1,23 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+$rootDir = dirname(dirname(__DIR__));
+
+return array (
+  'craftcms/redactor' => 
+  array (
+    'class' => 'craft\\redactor\\Plugin',
+    'basePath' => $vendorDir . '/craftcms/redactor/src',
+    'handle' => 'redactor',
+    'aliases' => 
+    array (
+      '@craft/redactor' => $vendorDir . '/craftcms/redactor/src',
+    ),
+    'name' => 'Redactor',
+    'version' => '2.6.0.1',
+    'description' => 'Edit rich text content in Craft CMS using Redactor by Imperavi.',
+    'developer' => 'Pixel & Tonic',
+    'developerUrl' => 'https://pixelandtonic.com/',
+    'documentationUrl' => 'https://github.com/craftcms/redactor/blob/v2/README.md',
+  ),
+);
